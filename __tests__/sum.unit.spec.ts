23cfbd49459071aeddd87c4ad544751843ce6533
higor-santosJ <higorsantos@allugator.com>
2023-05-26 17:54:03 -0300

    fix: test

--- __tests__/sum.unit.spec.ts
+++ __tests__/sum.unit.spec.ts
@@ -6,7 +6,7 @@ describe('sum module', () => {
   it('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
-  it('fail', () => {
-    expect(sum(1, 3)).toBe(3);
-  });
+  // it('fail', () => {
+  //   expect(sum(1, 3)).toBe(3);
+  // });
 });
