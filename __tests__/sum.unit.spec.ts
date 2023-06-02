eb7da581a68aadaeb2edbab8ba370178a4391e75
higor-santosJ <higorsantos@allugator.com>
2023-06-01 22:52:59 -0300

    feat: try to not stop and block pr

--- __tests__/sum.unit.spec.ts
+++ __tests__/sum.unit.spec.ts
@@ -7,7 +7,7 @@ describe('sum module', () => {
     expect(sum(1, 2)).toBe(3);
   });
 
-  it('adds 1 + 2 not to equal 3', () => {
-    expect(sum(1, 2)).not.toBe(3);
+  it('adds 1 + 2 to equal 3 - fail', () => {
+    expect(sum(1, 3)).toBe(3);
   });
 });
