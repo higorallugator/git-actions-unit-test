2be01fdc6d2adb67b56564c8a4cfc69194bbfe99
higor-santosJ <higorsantos@allugator.com>
2023-06-04 00:16:27 -0300

    feat: update workflow

---
+++ jest.int.config.js
@@ -0,0 +1,17 @@
+const rootConfig = require('./jest.config.js');
+
+module.exports = {
+  ...rootConfig,
+  ...{
+    displayName: 'Integration Tests',
+    coveragePathIgnorePatterns: [
+      '/node_modules/',
+      'src/api/',
+      '__tests__/',
+    ],
+    setupFilesAfterEnv: ['<rootDir>/__tests__/setup.ts'],
+    testMatch: [
+      '**/__tests__/**/*.int.(spec|test).[jt]s?(x)',
+    ],
+  },
+};
